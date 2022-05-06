627342b5a3841633c80c40ec94fb4e20bce261d2
xmq291 <xmq291@gmail.com>
2022-05-07 03:04:04 +0800

    8.4 Tether Smart Contract Shell

---
+++ src/contracts/Tether.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+contract Tether {
+    string public name  = "Tether";
+    string public symbol  = "USDT";
+    uint256 public totalSupply  = 1000000000000000000; // 1 milliion tokens   
+    uint8 public decimals = 18;
+}
