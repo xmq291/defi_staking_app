e1527a361175f767828891db321b31a9250ed4de
xmq291 <xmq291@gmail.com>
2022-05-07 02:15:51 +0800

    8.1 Truffle Configurations

---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+import "babel-register";
+import "babel-polyfill";
+
+module.exports = {
+    networks: {
+        development: {
+            host: "http://127.0.0.1",
+            port: 7545,
+            network_id: '*' // connect to any network
+        },
+    },
+    contract_directory: "./src/contracts",
+    contract_build_directory: "./src/truffle_abis",
+    compilers: {
+        solc: {
+            version: "^0.5.0",
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
+}
